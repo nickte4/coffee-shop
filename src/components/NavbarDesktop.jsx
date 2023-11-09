af4671f5508bea697f42b38a3dd515362db81c6c
Nick Topacio <topacio.3@osu.edu>
2023-11-09 10:23:34 -0500

    Modify desktop navbar

--- src/components/NavbarDesktop.jsx
+++ src/components/NavbarDesktop.jsx
@@ -4,20 +4,20 @@ export default function NavbarDesktop() {
   return (
     <>
       <div className="w-screen transition-all fixed bg-background h-24 z-10 shadow-2xl mb-5">
-        <ul className="flex justify-center content-center gap-10 my-8 text-2xl">
+        <ul className="flex justify-evenly content-center my-8 text-2xl w-screen">
           <a href="/">
             <LocalCafeIcon
               fontSize="large"
-              className="hover:text-primary transition-all cursor-pointer mr-80"
+              className="hover:text-primary transition-all cursor-pointer mr-48"
             />
           </a>
-          <li className="hover:underline underline-offset-2 hover:text-primary transition-all">
+          <li className="hover:border-b-2 border-primary hover:text-primary transition-all">
             <a href="/">Home</a>
           </li>
-          <li className="hover:underline underline-offset-2 hover:text-primary transition-all">
+          <li className="hover:border-b-2 border-primary hover:text-primary transition-all">
             <a href="/shop">Shop</a>
           </li>
-          <li className="hover:underline underline-offset-2 hover:text-primary transition-all">
+          <li className="hover:border-b-2 border-primary hover:text-primary transition-all">
             <a href="/about">About</a>
           </li>
         </ul>
