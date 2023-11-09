57e72f0026d7b873fa4686a76d0003ca60d41c35
Nick Topacio <topacio.3@osu.edu>
2023-11-09 10:46:16 -0500

    Add home image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 export default function Home() {
   return (
     <>
-      <div className="py-32 px-10">
-        <h1>Home</h1>
+      <div className="pt-16 z-0 min-h-screen">
+        <div className="relative">
+          <img
+            className="h-1/2 w-screen"
+            src="/home-image.jpg"
+            alt="coffee image"
+          />
+          <button className="w-28 h-16 text-2xl rounded-md bg-background transition-all duration-200 absolute bottom-32 left-16 hover:bg-primary hover:text-white">
+            Shop
+          </button>
+        </div>
+        <h1>home</h1>
       </div>
     </>
   );
