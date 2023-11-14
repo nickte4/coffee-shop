78366d6edb273c1400eb1dba1c58acbfc2c1d06c
Nick Topacio <topacio.3@osu.edu>
2023-11-14 17:46:33 -0500

    Space out links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,22 +12,25 @@ export default function Footer() {
             <h1 className="mt-2 text-lg">Untitled Coffee</h1>
           </a>
         </div>
-        <div>
+        <div className="flex justify-evenly gap-5">
           <a href="/shop">
             <h3 className="transition-all duration-200 text-2xl hover:border-b-2 w-max border-primary">
               Shop
             </h3>
           </a>
+          <span className="text-2xl">|</span>
           <a href="/about">
             <h3 className="transition-all duration-200 text-2xl hover:border-b-2 w-max border-primary border-spacing-2">
               About
             </h3>
           </a>
+          <span className="text-2xl">|</span>
           <a href="/contact">
             <h3 className="transition-all duration-200 text-2xl hover:border-b-2 w-max border-primary border-spacing-2">
               Contact
             </h3>
           </a>
+          <span className="text-2xl">|</span>
           <a href="/faq">
             <h3 className="transition-all duration-200 text-2xl hover:border-b-2 w-max border-primary border-spacing-2">
               FAQ
